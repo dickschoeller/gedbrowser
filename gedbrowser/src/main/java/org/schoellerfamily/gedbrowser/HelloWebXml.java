ee943d74f46e833f15496df37d0077aea16b6c45
Richard Schoeller <schoeller@comcast.net>
2016-12-26 20:23:12 -0500

    deprecations and versions

--- gedbrowser/src/main/java/org/schoellerfamily/gedbrowser/HelloWebXml.java
+++ gedbrowser/src/main/java/org/schoellerfamily/gedbrowser/HelloWebXml.java
@@ -3,7 +3,7 @@ package org.schoellerfamily.gedbrowser;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author Dick Schoeller
