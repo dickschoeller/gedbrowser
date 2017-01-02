88995e6b913dd8df5cc4715c9268925af1ff223a
Richard Schoeller <schoeller@comcast.net>
2017-01-02 15:23:02 -0500

    better health

--- gedbrowser/src/main/java/org/schoellerfamily/gedbrowser/endpoint/ApplicationHealthIndicator.java
+++ gedbrowser/src/main/java/org/schoellerfamily/gedbrowser/endpoint/ApplicationHealthIndicator.java
@@ -1,6 +1,18 @@
 package org.schoellerfamily.gedbrowser.endpoint;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.schoellerfamily.gedbrowser.controller.ApplicationInfo;
+import org.schoellerfamily.gedbrowser.datamodel.FinderStrategy;
+import org.schoellerfamily.gedbrowser.datamodel.Root;
+import org.schoellerfamily.gedbrowser.persistence.mongo.domain.GedDocumentMongoFactory;
+import org.schoellerfamily.gedbrowser.persistence.mongo.domain.RootDocumentMongo;
+import org.schoellerfamily.gedbrowser.persistence.mongo.repository.RootDocumentRepositoryMongo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.Health.Builder;
@@ -14,18 +26,53 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ApplicationHealthIndicator implements HealthIndicator {
+    /** Logger. */
+    private final transient Log logger = LogFactory.getLog(getClass());
+
     /** */
     @Autowired
     private transient ApplicationInfo appInfo;
+
+    /** */
+    @Autowired
+    private transient RootDocumentRepositoryMongo rootDocumentRepository;
+
+    /** */
+    @Autowired
+    private transient FinderStrategy finder;
 
     /**
      * {@inheritDoc}
      */
     @Override
     public Health health() {
-        final Builder upBuilder = Health.up();
-        upBuilder.withDetail("version", appInfo.getVersion());
-        // TODO add more stuff about databases here.
-        return upBuilder.build();
+        logger.info("Add to health report");
+        final Builder builder = Health.up();
+        builder.withDetail("version", appInfo.getVersion());
+        // TODO this shouldn't be bound to mongo. We need to bury that.
+        final List<Map<String, Object>> datasetList = new ArrayList<>();
+        for (final RootDocumentMongo mongo
+                : rootDocumentRepository.findAll()) {
+            logger.info("add " + mongo.getDbName()
+                + " to datasets in health report");
+            datasetList.add(createDetail(mongo));
+        }
+        builder.withDetail("datasets", datasetList);
+        builder.up();
+        return builder.build();
+    }
+
+    /**
+     * @param mongo the current database
+     * @return the details
+     */
+    private Map<String, Object> createDetail(final RootDocumentMongo mongo) {
+        final Root root = (Root) GedDocumentMongoFactory.getInstance().
+                createRoot(mongo, finder);
+        mongo.setGedObject(root);
+        final Map<String, Object> detailsMap = new HashMap<>();
+        detailsMap.put("dbname", mongo.getDbName());
+        detailsMap.put("filename", mongo.getFilename());
+        return detailsMap;
     }
 }
