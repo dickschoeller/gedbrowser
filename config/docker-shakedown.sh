2aa3d8e7177d4e10724f0c225c7ccac6e92d54d8
Richard Schoeller <schoeller@comcast.net>
2017-02-09 15:59:49 -0500

    try to fix problems

--- config/docker-shakedown.sh
+++ config/docker-shakedown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --link mongo:mongo -v /home/travis/gedbrowser:/var/lib/gedbrowser -p 8086:8080 -p 8087:8080 --name geoservice -d dickschoeller/geoservice:snapshot
-docker run --link mongo:mongo -v /home/travis/gedbrowser:/var/lib/gedbrowser -p 8080:8080 -p 8081:8081 --name gedbrowser -d dickschoeller/gedbrowser:snapshot
+docker run --link mongo:mongo --link geoservice:geoservice -v /home/travis/gedbrowser:/var/lib/gedbrowser -p 8080:8080 -p 8081:8081 --name gedbrowser -d dickschoeller/gedbrowser:snapshot
 echo "Wait a minute"
 sleep 1m
 echo "Done waiting, let's see how Docker has done"
