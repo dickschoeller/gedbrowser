cdb8eafac40ed4cae79f801842d802ea0603abe6
Richard Schoeller <schoeller@comcast.net>
2017-02-09 16:53:35 -0500

    try, try again

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    echo $GPG_PASSPHRASE
     mvn --batch-mode --settings=config/settings.xml -Psign,deploy -DpushImage deploy
 else
     echo "Pull request, no deployment step"
