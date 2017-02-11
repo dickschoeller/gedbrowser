1f9bf96464468984355ac9d63eb42d4e3ce23862
Richard Schoeller <schoeller@comcast.net>
2017-02-11 12:39:49 -0500

    try running selenium local to travis

--- config/before-deploy.sh
+++ config/before-deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo TRAVIS_BRANCH=$TRAVIS_BRANCH
+echo TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST
 if [[ $TRAVIS_PULL_REQUEST == 'false' && ( $TRAVIS_BRANCH == 'master' || $TRAVIS_BRANCH == 'development' ) ]]; then
     openssl aes-256-cbc -K $encrypted_0b79b78d9b0a_key -iv $encrypted_0b79b78d9b0a_iv -in config/codesigning.asc.enc -out config/codesigning.asc -d
     gpg --fast-import config/codesigning.asc
