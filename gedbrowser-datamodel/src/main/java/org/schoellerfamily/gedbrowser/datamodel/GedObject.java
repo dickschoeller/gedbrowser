9f320cae84510df2ec4c4cfaea9b10e7da81efe7
Richard Schoeller <schoeller@comcast.net>
2017-04-28 00:12:42 -0400

    missed some version changes

--- gedbrowser-datamodel/src/main/java/org/schoellerfamily/gedbrowser/datamodel/GedObject.java
+++ gedbrowser-datamodel/src/main/java/org/schoellerfamily/gedbrowser/datamodel/GedObject.java
@@ -20,7 +20,7 @@ public abstract class GedObject {
     /** */
     protected static final String DEFAULT_IDX_NAME = "";
     /** */
-    public static final String VERSION = "1.1.2-SNAPSHOT";
+    public static final String VERSION = "1.1.2";
     /** */
     protected static final String DEFAULT_SURNAME = "";
     /** */
