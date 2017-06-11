63680c1c5fe7d9936ff6a78bce025e94dbf37933
Richard Schoeller <schoeller@comcast.net>
2017-06-11 17:41:59 -0400

    Fixes #400 - Clean up SubmittorsControllerTest
    Did some checking that the page contents are as expected.

--- gedbrowser/src/test/java/org/schoellerfamily/gedbrowser/controller/test/SubmittorsControllerTest.java
+++ gedbrowser/src/test/java/org/schoellerfamily/gedbrowser/controller/test/SubmittorsControllerTest.java
@@ -44,10 +44,23 @@ public class SubmittorsControllerTest {
                 String.class);
 
         then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        then(entity.getBody()).contains("<title>Surnames</title>")
-//            .contains("<span><a href=\"surnames?db=gl120368&amp;letter=?\" cl"
-//                    + "ass=\"name\">[?]</a>   </span>")
-//            .contains("<li><a href=\"person?db=gl120368&amp;id=");
-    }
+        then(entity.getBody()).contains("<title>Submittors</title>")
+            .contains("Submittors for dataset: gl120368</h2>")
+            .contains("href=\"submittor?db=gl120368&amp;id=U1\">Phil Williams")
+            .contains("href=\"submittor?db=gl120368&amp;id=U3\"> ")
+            .contains("href=\"submittor?db=gl120368&amp;id=U14\">A K Robinson")
+            .contains("href=\"submittor?db=gl120368&amp;id=U15\">Michael ")
+            .contains("href=\"submittor?db=gl120368&amp;id=U2\">Arthur  ")
+            .contains("href=\"submittor?db=gl120368&amp;id=U4\">Created by")
+            .contains("href=\"submittor?db=gl120368&amp;id=U5\">Paul Alger")
+            .contains("href=\"submittor?db=gl120368&amp;id=U6\">Jim Beecroft")
+            .contains("href=\"submittor?db=gl120368&amp;id=U7\">ken stel")
+            .contains("href=\"submittor?db=gl120368&amp;id=U8\">Mark Willis B")
+            .contains("href=\"submittor?db=gl120368&amp;id=U9\">Patricia Fisc")
+            .contains("href=\"submittor?db=gl120368&amp;id=U10\">Hirt-Klooze")
+            .contains("href=\"submittor?db=gl120368&amp;id=U11\">Lester LeMay")
+            .contains("href=\"submittor?db=gl120368&amp;id=U12\">David A. Blo")
+            .contains("href=\"submittor?db=gl120368&amp;id=U13\">Dave Morris");
+        }
 
 }
