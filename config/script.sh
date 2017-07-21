03ea9c7d3c8db9cf7f0fa9142b2db3f5e5ff5322
Richard Schoeller <schoeller@comcast.net>
2017-07-21 18:02:36 -0400

    more work on travis tests

--- config/script.sh
+++ config/script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn install jacoco:report coveralls:report --settings=config/settings.xml -Ptravis-build -DrepoToken=${REPOTOKEN} -B -Dspring.data.mongodb.port=28001 -Dgedbrowser.home=/home/travis/gedbrowser -Dgeoservice.keyfile=stub -Dgeoservice.backupfile=/home/travis/gedbrowser/geoservice-test-backup.json -DpushImage -DpushImageTag
+mvn install jacoco:report coveralls:report --settings=config/settings.xml -Ptravis-build -DrepoToken=${REPOTOKEN} -B -Dspring.data.mongodb.port=28001 -Dgedbrowser.home=/home/travis/gedbrowser -Dgeoservice.keyfile=stub -Dgeoservice.backupfile=/home/travis/gedbrowser/geoservice-test-backup.json -Dgeoservice.loadfile=/home/travis/gedbrowser/geoservice-loadfile.txt -DpushImage -DpushImageTag
