31deeffddbec1e998419e1203f4d682933127a1f
Richard Schoeller <schoeller@comcast.net>
2017-07-23 17:02:35 -0400

    log to try to figure out wtf

--- gedbrowser/src/test/java/org/schoellerfamily/gedbrowser/selenium/pageobjects/LoginPage.java
+++ gedbrowser/src/test/java/org/schoellerfamily/gedbrowser/selenium/pageobjects/LoginPage.java
@@ -1,9 +1,16 @@
 package org.schoellerfamily.gedbrowser.selenium.pageobjects;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.WebElement;
+
 /**
  * @author Dick Schoeller
  */
 public class LoginPage extends PageBase {
+    /** Logger. */
+    private final transient Log logger = LogFactory.getLog(getClass());
+
     /**
      * @param factory the factory for creating pages as we navigate
      * @param previous the previous page
@@ -20,9 +27,16 @@ public class LoginPage extends PageBase {
      * @return the page we got here from
      */
     public PageBase login(final String username, final String password) {
+        logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        logger.info("username: " + username + " password: " + password);
+        logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++");
         sendText("input[name='username']", username);
         sendText("input[name='password']", password);
-        getWebElement("input[value='Login']").click();
+        final WebElement webElement = getWebElement("input[value='Login']");
+        logger.info("webelement: " + webElement);
+        webElement.click();
         waitForPageLoaded();
         return getPrevious();
     }
