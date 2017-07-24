4b472d5d3b9cced0c413849207384dd7b346ae77
Richard Schoeller <schoeller@comcast.net>
2017-07-23 23:27:59 -0400

    another workaround try

--- gedbrowser/src/test/java/org/schoellerfamily/gedbrowser/selenium/pageobjects/LoginPage.java
+++ gedbrowser/src/test/java/org/schoellerfamily/gedbrowser/selenium/pageobjects/LoginPage.java
@@ -1,16 +1,11 @@
 package org.schoellerfamily.gedbrowser.selenium.pageobjects;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.WebElement;
 
 /**
  * @author Dick Schoeller
  */
 public class LoginPage extends PageBase {
-    /** Logger. */
-    private final transient Log logger = LogFactory.getLog(getClass());
-
     /**
      * @param factory the factory for creating pages as we navigate
      * @param previous the previous page
@@ -27,37 +22,15 @@ public class LoginPage extends PageBase {
      * @return the page we got here from
      */
     public PageBase login(final String username, final String password) {
-        logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        logger.info("username: " + username + " password: " + password);
-        logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        try {
-            sendText("input[name='username']", username);
-        } catch (Exception e) {
-            logger.error("username", e);
-            throw e;
-        }
-        try {
-            sendText("input[name='password']", password);
-        } catch (Exception e) {
-            logger.error("password", e);
-            throw e;
-        }
-        try {
-            final WebElement webElement = getWebElement("input[value='Login']");
-            logger.info("webelement: " + webElement);
-            webElement.click();
-        } catch (Exception e) {
-            logger.error("click button", e);
-            throw e;
-        }
-        try {
-            waitForPageLoaded();
-        } catch (Exception e) {
-            logger.error("page load", e);
-            throw e;
-        }
+        getWebElement("input[name='username'").click();
+        getWebElement("input[name='username'").clear();
+        sendText("input[name='username']", username);
+        getWebElement("input[name='password'").click();
+        getWebElement("input[name='password'").clear();
+        sendText("input[name='password']", password);
+        final WebElement webElement = getWebElement("input[value='Login']");
+        webElement.click();
+        waitForPageLoaded();
         return getPrevious();
     }
 }
