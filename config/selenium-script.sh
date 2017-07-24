93eb96cc919170d2977fdba25a934211246457ec
Richard Schoeller <schoeller@comcast.net>
2017-07-23 22:52:13 -0400

    be smarter about version argument

--- config/selenium-script.sh
+++ config/selenium-script.sh
@@ -6,4 +6,11 @@ echo TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}
 echo TRAVIS_JOB_NUMBER=${TRAVIS_JOB_NUMBER}
 echo browser=${browser}
 echo platform=${platform}
-mvn integration-test verify ${INTEGRATION_TEST_ARGS} -Dselenium.browser.name=${browser} -Dselenium.platform=${platform} -Dselenium.browser.version=${version} -Dselenium.timeout=120
+echo version=${version}
+if [ -z $version ]; then
+    varg=""
+else
+    varg="-Dselenium.browser.version=${version}"
+fi
+echo mvn integration-test verify ${INTEGRATION_TEST_ARGS} -Dselenium.browser.name=${browser} -Dselenium.platform=${platform} ${varg} -Dselenium.timeout=120
+mvn integration-test verify ${INTEGRATION_TEST_ARGS} -Dselenium.browser.name=${browser} -Dselenium.platform=${platform} ${varg} -Dselenium.timeout=120
