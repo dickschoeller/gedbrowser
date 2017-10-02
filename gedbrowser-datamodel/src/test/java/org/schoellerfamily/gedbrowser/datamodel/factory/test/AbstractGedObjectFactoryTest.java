1437d902bde94f98d6d57016f46aa3c60dcc6f34
Richard Schoeller <schoeller@comcast.net>
2017-10-02 00:14:18 -0400

    fix the next oops

--- gedbrowser-datamodel/src/test/java/org/schoellerfamily/gedbrowser/datamodel/factory/test/AbstractGedObjectFactoryTest.java
+++ gedbrowser-datamodel/src/test/java/org/schoellerfamily/gedbrowser/datamodel/factory/test/AbstractGedObjectFactoryTest.java
@@ -42,6 +42,117 @@ import org.schoellerfamily.gedobject.datamodel.factory.AbstractGedObjectFactory.
 @RunWith(Parameterized.class)
 @SuppressWarnings("PMD.ExcessiveImports")
 public final class AbstractGedObjectFactoryTest {
+
+    /** */
+    private static final GedObjectBuilder BUILDER = new GedObjectBuilder();
+
+    /** */
+    private static final Root ROOT = BUILDER.getRoot();
+
+    /** */
+    private static final Person PERSON = BUILDER.createPerson(
+            "I1", "Richard John/Schoeller/");
+
+    /** */
+    private static final Attribute NOTE =
+            new Attribute(PERSON, "Note", "Some text");
+
+    /** */
+    private static final Object[][] PARAMETERS = {
+            {null, null, "ROOT", null, new Root("Root")},
+            {null, "", "ROOT", null, new Root("Root")},
+            {null, null, "ROOT", "", new Root("Root")},
+            {null, "", "ROOT", "", new Root("Root")},
+            {ROOT, "", "ROOT", "", new Root("Root")},
+            {null, null, null, null, new Attribute()},
+            {null, "", "", "", new Attribute()},
+            {null, null, "ATTRIBUTE", null,
+                new Attribute(null, "Attribute", null)},
+            {null, "", "ATTRIBUTE", "", new Attribute(null, "Attribute", "")},
+            {PERSON, null, null, null, new Attribute(PERSON)},
+            {PERSON, "", "", "", new Attribute(PERSON)},
+            {PERSON, null, "ATTRIBUTE", null,
+                new Attribute(PERSON, "Attribute", null)},
+            {PERSON, "", "ATTRIBUTE", "",
+                new Attribute(PERSON, "Attribute", "")},
+            {null, "", "BIRT", null, new Attribute(null, "Birth", null)},
+            {null, null, "INDI", null, new Person()},
+            {null, "I1", "INDI", null, new Person(null, new ObjectId("I1"))},
+            {PERSON, "", "ATTRIBUTE", "",
+                new Attribute(PERSON, "Attribute", "")},
+            {PERSON, "", "BIRT", null, new Attribute(PERSON, "Birth", null)},
+            {null, null, "FAMS", "F1",
+                new FamS(null, "Spouse of Family", new ObjectId("F1"))},
+            {null, null, "FAMC", "F1",
+                new FamC(null, "Child of Family", new ObjectId("F1"))},
+            {null, null, "LINK", "I1",
+                    new Link(null, "Link", new ObjectId("I1"))},
+            {null, null, "FAM", null, new Family()},
+            {null, "F1", "FAM", null, new Family(null, new ObjectId("F1"))},
+            {null, null, "HUSB", "I1",
+                new Husband(null, "Husband", new ObjectId("I1"))},
+            {null, null, "WIFE", "I2",
+                new Wife(null, "Wife", new ObjectId("I2"))},
+            {null, null, "CHIL", "I1",
+                new Child(null, "Child", new ObjectId("I1"))},
+            {ROOT, "S1", "SOUR", null,
+                new Source(ROOT, new ObjectId("S1"))},
+            {PERSON, null, "SOUR", "@S1@",
+                new SourceLink(PERSON, "Source", new ObjectId("S1"))},
+            {PERSON, null, "SOUR", "Source text",
+                new Attribute(PERSON, "Source", "Source text")},
+            {ROOT, "SUB1", "SUBM", null,
+                new Submitter(ROOT, new ObjectId("SUB1"))},
+            {PERSON, null, "SUBM", "@SUB1@",
+                new SubmitterLink(PERSON, "Submitter", new ObjectId("SUB1"))},
+            {PERSON, null, "SUBM", "SUB1",
+                new SubmitterLink(PERSON, "Submitter", new ObjectId("SUB1"))},
+            {ROOT, "SUBN", "SUBN", null,
+                new Submission(ROOT, new ObjectId("SUBN"))},
+            {PERSON, null, "SUBN", "@SUBN@",
+                new SubmissionLink(PERSON, "Submission", new ObjectId("SUBN"))},
+            {PERSON, null, "SUBN", "SUBN",
+                new SubmissionLink(PERSON, "Submission", new ObjectId("SUBN"))},
+            {null, null, "DATE", null, new Date(null)},
+            {PERSON, null, "DATE", null, new Date(PERSON)},
+            {PERSON, null, "DATE", "1 JAN 1950",
+                new Date(PERSON, "1 JAN 1950")},
+            {null, null, "NAME", null, new Name()},
+            {PERSON, null, "NAME", null, new Name(PERSON)},
+            {PERSON, null, "NAME", "John/Smith/",
+                new Name(PERSON, "John/Smith/")},
+            {null, null, "PLAC", null, new Place(null, null)},
+            {PERSON, null, "PLAC", null, new Place(PERSON, null)},
+            {PERSON, null, "PLAC", "Home", new Place(PERSON, "Home")},
+            {null, null, "TRLR", null, new Trailer(null, "Trailer")},
+            {ROOT, null, "TRLR", null, new Trailer(ROOT, "Trailer")},
+            {ROOT, null, "HEAD", "", new Head(ROOT, "Header")},
+            {ROOT, null, "HEAD", "", new Head(ROOT, "Header")},
+            {ROOT, null, "HEAD", "tail", new Head(ROOT, "Header", "tail")},
+            {PERSON, null, "NOTE", "", new Attribute(PERSON, "Note", "")},
+            {PERSON, null, "NOTE", "Some text",
+                new Attribute(PERSON, "Note", "Some text")},
+            {ROOT, "N1", "NOTE", "Some text",
+                new Note(ROOT, new ObjectId("N1"), "Some text")},
+            {PERSON, null, "NOTE", "@N1@",
+                new NoteLink(PERSON, "Note", new ObjectId("N1"))},
+            {PERSON, null, "OBJE", null, new Multimedia(PERSON, "Multimedia")},
+            {PERSON, null, "OBJE", null,
+                new Multimedia(PERSON, "Multimedia", null)},
+            {PERSON, null, "OBJE", "string",
+                new Multimedia(PERSON, "Multimedia", "string")},
+            {PERSON, "foo", "OBJE", null,
+                new Multimedia(PERSON, "Multimedia", null)},
+            // These 2 are funky
+            {NOTE, null, "CONC", "y stuff", null},
+            {NOTE, null, "CONT", "and some more text", null},
+//            {note, null, "CONC", "y stuff",
+//                new Attribute(person, "Note", "Some texty stuff")},
+//            {note, null, "CONT", "and some more text",
+//                new Attribute(person, "Note",
+//                    "Some text\nand some more text")},
+        };
+
     /** */
     private GedObjectFactory factory;
     /** */
@@ -77,113 +188,14 @@ public final class AbstractGedObjectFactoryTest {
         this.tail = tail;
         this.expected = expected;
     }
-
     /**
      * @return the collection of test parameters
      */
     @Parameters
+    @SuppressWarnings("PMD.MethodReturnsInternalArray")
     public static Object[][] params() {
-        final GedObjectBuilder builder = new GedObjectBuilder();
-
-        final Root root = builder.getRoot();
-        final Person person = builder.createPerson(
-                "I1", "Richard John/Schoeller/");
-        final Attribute note = new Attribute(person, "Note", "Some text");
-        person.addAttribute(note);
-        return new Object[][] {
-            {null, null, "ROOT", null, new Root("Root")},
-            {null, "", "ROOT", null, new Root("Root")},
-            {null, null, "ROOT", "", new Root("Root")},
-            {null, "", "ROOT", "", new Root("Root")},
-            {root, "", "ROOT", "", new Root("Root")},
-            {null, null, null, null, new Attribute()},
-            {null, "", "", "", new Attribute()},
-            {null, null, "ATTRIBUTE", null,
-                new Attribute(null, "Attribute", null)},
-            {null, "", "ATTRIBUTE", "", new Attribute(null, "Attribute", "")},
-            {person, null, null, null, new Attribute(person)},
-            {person, "", "", "", new Attribute(person)},
-            {person, null, "ATTRIBUTE", null,
-                new Attribute(person, "Attribute", null)},
-            {person, "", "ATTRIBUTE", "",
-                new Attribute(person, "Attribute", "")},
-            {null, "", "BIRT", null, new Attribute(null, "Birth", null)},
-            {null, null, "INDI", null, new Person()},
-            {null, "I1", "INDI", null, new Person(null, new ObjectId("I1"))},
-            {person, "", "ATTRIBUTE", "",
-                new Attribute(person, "Attribute", "")},
-            {person, "", "BIRT", null, new Attribute(person, "Birth", null)},
-            {null, null, "FAMS", "F1",
-                new FamS(null, "Spouse of Family", new ObjectId("F1"))},
-            {null, null, "FAMC", "F1",
-                new FamC(null, "Child of Family", new ObjectId("F1"))},
-            {null, null, "LINK", "I1",
-                    new Link(null, "Link", new ObjectId("I1"))},
-            {null, null, "FAM", null, new Family()},
-            {null, "F1", "FAM", null, new Family(null, new ObjectId("F1"))},
-            {null, null, "HUSB", "I1",
-                new Husband(null, "Husband", new ObjectId("I1"))},
-            {null, null, "WIFE", "I2",
-                new Wife(null, "Wife", new ObjectId("I2"))},
-            {null, null, "CHIL", "I1",
-                new Child(null, "Child", new ObjectId("I1"))},
-            {root, "S1", "SOUR", null,
-                new Source(root, new ObjectId("S1"))},
-            {person, null, "SOUR", "@S1@",
-                new SourceLink(person, "Source", new ObjectId("S1"))},
-            {person, null, "SOUR", "Source text",
-                new Attribute(person, "Source", "Source text")},
-            {root, "SUB1", "SUBM", null,
-                new Submitter(root, new ObjectId("SUB1"))},
-            {person, null, "SUBM", "@SUB1@",
-                new SubmitterLink(person, "Submitter", new ObjectId("SUB1"))},
-            {person, null, "SUBM", "SUB1",
-                new SubmitterLink(person, "Submitter", new ObjectId("SUB1"))},
-            {root, "SUBN", "SUBN", null,
-                new Submission(root, new ObjectId("SUBN"))},
-            {person, null, "SUBN", "@SUBN@",
-                new SubmissionLink(person, "Submission", new ObjectId("SUBN"))},
-            {person, null, "SUBN", "SUBN",
-                new SubmissionLink(person, "Submission", new ObjectId("SUBN"))},
-            {null, null, "DATE", null, new Date(null)},
-            {person, null, "DATE", null, new Date(person)},
-            {person, null, "DATE", "1 JAN 1950",
-                new Date(person, "1 JAN 1950")},
-            {null, null, "NAME", null, new Name()},
-            {person, null, "NAME", null, new Name(person)},
-            {person, null, "NAME", "John/Smith/",
-                new Name(person, "John/Smith/")},
-            {null, null, "PLAC", null, new Place(null, null)},
-            {person, null, "PLAC", null, new Place(person, null)},
-            {person, null, "PLAC", "Home", new Place(person, "Home")},
-            {null, null, "TRLR", null, new Trailer(null, "Trailer")},
-            {root, null, "TRLR", null, new Trailer(root, "Trailer")},
-            {root, null, "HEAD", "", new Head(root, "Header")},
-            {root, null, "HEAD", "", new Head(root, "Header")},
-            {root, null, "HEAD", "tail", new Head(root, "Header", "tail")},
-            {person, null, "NOTE", "", new Attribute(person, "Note", "")},
-            {person, null, "NOTE", "Some text",
-                new Attribute(person, "Note", "Some text")},
-            {root, "N1", "NOTE", "Some text",
-                new Note(root, new ObjectId("N1"), "Some text")},
-            {person, null, "NOTE", "@N1@",
-                new NoteLink(person, "Note", new ObjectId("N1"))},
-            {person, null, "OBJE", null, new Multimedia(person, "Multimedia")},
-            {person, null, "OBJE", null,
-                new Multimedia(person, "Multimedia", null)},
-            {person, null, "OBJE", "string",
-                new Multimedia(person, "Multimedia", "string")},
-            {person, "foo", "OBJE", null,
-                new Multimedia(person, "Multimedia", null)},
-            // These 2 are funky
-            {note, null, "CONC", "y stuff", null},
-            {note, null, "CONT", "and some more text", null},
-//            {note, null, "CONC", "y stuff",
-//                new Attribute(person, "Note", "Some texty stuff")},
-//            {note, null, "CONT", "and some more text",
-//                new Attribute(person, "Note",
-//                    "Some text\nand some more text")},
-        };
+        PERSON.addAttribute(NOTE);
+        return PARAMETERS;
     }
 
     /** */
