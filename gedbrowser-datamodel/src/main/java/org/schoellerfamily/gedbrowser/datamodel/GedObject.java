cbd5a51e8dc586e45e73796167e489c97cf708fb
Richard Schoeller <schoeller@comcast.net>
2018-03-07 18:31:42 -0500

    change the release target to 1.3.0-M1

--- gedbrowser-datamodel/src/main/java/org/schoellerfamily/gedbrowser/datamodel/GedObject.java
+++ gedbrowser-datamodel/src/main/java/org/schoellerfamily/gedbrowser/datamodel/GedObject.java
@@ -16,7 +16,7 @@ public abstract class GedObject extends AbstractFinderObject
     /** */
     protected static final String DEFAULT_IDX_NAME = "";
     /** */
-    public static final String VERSION = "1.2.3-SNAPSHOT";
+    public static final String VERSION = "1.3.0-M1-SNAPSHOT";
     /** */
     protected static final String DEFAULT_SURNAME = "";
     /** */
