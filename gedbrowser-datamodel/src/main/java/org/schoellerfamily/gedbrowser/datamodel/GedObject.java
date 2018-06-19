b5c41af92de5407b14f5ac1ac2ba2d888b924ea2
Richard Schoeller <schoeller@comcast.net>
2018-06-19 08:12:43 -0400

    Release 1.3.0-M3
    * Fixes #701
    * Change from Angular Materials to PrimeNG
    * Major expansion of the editing functions in gedbrowserng
      * Drag and drop ordering
      * Create, link and unlink in every relation to person
      * Link sources
      * Multiple GEDCOMs (a hack)

--- gedbrowser-datamodel/src/main/java/org/schoellerfamily/gedbrowser/datamodel/GedObject.java
+++ gedbrowser-datamodel/src/main/java/org/schoellerfamily/gedbrowser/datamodel/GedObject.java
@@ -16,7 +16,7 @@ public abstract class GedObject extends AbstractFinderObject
     /** */
     protected static final String DEFAULT_IDX_NAME = "";
     /** */
-    public static final String VERSION = "1.3.0-M3-SNAPSHOT";
+    public static final String VERSION = "1.3.0-M3";
     /** */
     protected static final String DEFAULT_SURNAME = "";
     /** */
