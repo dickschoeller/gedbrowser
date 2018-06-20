fa2017e8834ba6eb9df96e8291600b3351c7760f
Richard Schoeller <schoeller@comcast.net>
2018-06-19 23:28:57 -0400

    reduce code duplication

--- gedbrowserng-frontend/src/app/utils/api-comparators.ts
+++ gedbrowserng-frontend/src/app/utils/api-comparators.ts
@@ -1,27 +1,23 @@
 import { ApiSource, ApiPerson, ApiSubmitter } from '../models';
 
 export class ApiComparators {
-  comparePersons(a: ApiPerson, b: ApiPerson) {
-    const val = a.indexName.localeCompare(b.indexName);
-    if (val !== 0) {
-      return val;
-    }
-    return a.string.localeCompare(b.string);
+  comparePersons = (a: ApiPerson, b: ApiPerson) => {
+    return this.compare(a.indexName, a.string, b.indexName, b.string);
   }
 
-  compareSources(a: ApiSource, b: ApiSource) {
-    const val = a.title.localeCompare(b.title);
-    if (val !== 0) {
-      return val;
-    }
-    return a.string.localeCompare(b.string);
+  compareSources = (a: ApiSource, b: ApiSource) => {
+    return this.compare(a.title, a.string, b.title, b.string);
   }
 
-  compareSubmitters(a: ApiSubmitter, b: ApiSubmitter) {
-    const val = a.name.localeCompare(b.name);
+  compareSubmitters = (a: ApiSubmitter, b: ApiSubmitter) => {
+    return this.compare(a.name, a.string, b.name, b.string);
+  }
+
+  compare = (a1: string, a2: string, b1: string, b2: string) => {
+    const val = a1.localeCompare(b1);
     if (val !== 0) {
       return val;
     }
-    return a.string.localeCompare(b.string);
+    return a2.localeCompare(b2);
   }
 }
