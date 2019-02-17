e3a3a3e94698f212e46779bdad1ab219113955ca
Richard Schoeller <schoeller@comcast.net>
2019-02-17 18:52:22 -0500

    Release 1.3.0-M6

--- geoservice/src/test/java/org/schoellerfamily/geoservice/test/ApplicationInfoTest.java
+++ geoservice/src/test/java/org/schoellerfamily/geoservice/test/ApplicationInfoTest.java
@@ -72,6 +72,6 @@ public final class ApplicationInfoTest {
     @Test
     public void testApplicationInfoVersion() {
         assertEquals("Version mismatch",
-                "1.3.0-M6-SNAPSHOT", appInfo.getVersion());
+                "1.3.0-M6", appInfo.getVersion());
     }
 }
