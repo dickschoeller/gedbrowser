57b735654045ddc852ee3df7c478a3e365d3b3bf
Richard Schoeller <schoeller@comcast.net>
2019-05-07 20:53:27 -0400

    break up configure to reduce complexity

--- gedbrowserng/src/main/java/org/schoellerfamily/gedbrowser/api/WebSecurityConfig.java
+++ gedbrowserng/src/main/java/org/schoellerfamily/gedbrowser/api/WebSecurityConfig.java
@@ -122,26 +122,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      * {@inheritDoc}
      */
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        handleCsrf(http)
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().exceptionHandling()
-                .authenticationEntryPoint(restAuthenticationEntryPoint)
-            .and().addFilterBefore(jwtAuthenticationTokenFilter(),
-                    BasicAuthenticationFilter.class)
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-            .and().formLogin()
-                .loginPage("/gedbrowserng/v1/login")
-                .successHandler(authenticationSuccessHandler)
-                .failureHandler(authenticationFailureHandler)
-            .and().logout()
-                .logoutRequestMatcher(
-                        new AntPathRequestMatcher("/gedbrowserng/v1/logout"))
-                .logoutSuccessHandler(logoutSuccess)
-                .deleteCookies(config.cookie());
+    protected void configure(final HttpSecurity httpSecurity) throws Exception {
+        HttpSecurity http = configureCsrf(httpSecurity);
+        http = configureSessionManagement(http);
+        http = configureAuthEntryPoint(http);
+        http = configureAuthenticationFilter(http);
+        http = configureHandleLogin(http);
+        http = configureHandleLogout(http);
     }
 
     /**
@@ -152,7 +139,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      * @return the http security object
      * @throws Exception if there is a problem
      */
-    private HttpSecurity handleCsrf(final HttpSecurity http)
+    private HttpSecurity configureCsrf(final HttpSecurity http)
             throws Exception {
         if ("test".equals(config.activeProfile())) {
             return http.csrf().disable();
@@ -164,6 +151,79 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                             CookieCsrfTokenRepository.withHttpOnlyFalse())
                     .and();
         }
+    }
+
+    /**
+     * @param http the http security object
+     * @return the http security object
+     * @throws Exception if there is a problem
+     */
+    private HttpSecurity configureSessionManagement(final HttpSecurity http)
+            throws Exception {
+        return http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+    }
+
+    /**
+     * @param http the http security object
+     * @return the http security object
+     * @throws Exception if there is a problem
+     */
+    private HttpSecurity configureAuthEntryPoint(final HttpSecurity http)
+            throws Exception {
+        return http
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and();
+    }
+
+    /**
+     * @param http the http security object
+     * @return the http security object
+     * @throws Exception if there is a problem
+     */
+    private HttpSecurity configureAuthenticationFilter(final HttpSecurity http)
+            throws Exception {
+        return http
+                .addFilterBefore(jwtAuthenticationTokenFilter(),
+                        BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and();
+    }
+
+    /**
+     * @param http the http security object
+     * @return the http security object
+     * @throws Exception if there is a problem
+     */
+    private HttpSecurity configureHandleLogin(final HttpSecurity http)
+            throws Exception {
+        return http.formLogin()
+                .loginPage("/gedbrowserng/v1/login")
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
+                .and();
+    }
+
+    /**
+     * @param http the http security object
+     * @return the http security object
+     * @throws Exception if there is a problem
+     */
+    private HttpSecurity configureHandleLogout(
+            final HttpSecurity http)
+            throws Exception {
+        return http
+                .logout()
+                .logoutRequestMatcher(
+                        new AntPathRequestMatcher("/gedbrowserng/v1/logout"))
+                .logoutSuccessHandler(logoutSuccess)
+                .deleteCookies(config.cookie())
+                .and();
     }
 
     /**
