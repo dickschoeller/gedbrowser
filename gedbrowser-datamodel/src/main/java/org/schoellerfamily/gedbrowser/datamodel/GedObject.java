357c6a6c0b8c21f7c5eb94de724b999b61cd835a
Richard Schoeller <schoeller@comcast.net>
2020-05-16 16:46:18 -0400

    Start work on 1.3.0-RC2

--- gedbrowser-datamodel/src/main/java/org/schoellerfamily/gedbrowser/datamodel/GedObject.java
+++ gedbrowser-datamodel/src/main/java/org/schoellerfamily/gedbrowser/datamodel/GedObject.java
@@ -16,7 +16,7 @@ public abstract class GedObject extends AbstractFinderObject
     /** */
     protected static final String DEFAULT_IDX_NAME = "";
     /** */
-    public static final String VERSION = "1.3.0-RC1";
+    public static final String VERSION = "1.3.0-RC2-SNAPSHOT";
     /** */
     protected static final String DEFAULT_SURNAME = "";
     /** */
--- geoservice/src/main/java/org/schoellerfamily/geoservice/controller/ApplicationInfo.java
+++ geoservice/src/main/java/org/schoellerfamily/geoservice/controller/ApplicationInfo.java
@@ -10,7 +10,7 @@ public final class ApplicationInfo {
     /** */
     private static final String NAME = "gedbrowser-geoservice";
     /** */
-    private static final String VERSION = "1.3.0-RC1";
+    private static final String VERSION = "1.3.0-RC2-SNAPSHOT";
     /** */
     private static final String APPLICATION_URL =
             "https://github.com/dickschoeller/gedbrowser";
