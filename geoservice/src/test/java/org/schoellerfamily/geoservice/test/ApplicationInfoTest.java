01ae71c0a6d8f42db0949df11c8c30b042faadb1
Richard Schoeller <schoeller@comcast.net>
2020-05-23 14:47:37 -0400

    Start 1.3.0-RC3 work

--- geoservice/src/test/java/org/schoellerfamily/geoservice/test/ApplicationInfoTest.java
+++ geoservice/src/test/java/org/schoellerfamily/geoservice/test/ApplicationInfoTest.java
@@ -72,6 +72,6 @@ public final class ApplicationInfoTest {
     @Test
     public void testApplicationInfoVersion() {
         assertEquals("Version mismatch",
-                "1.3.0-RC2", appInfo.getVersion());
+                "1.3.0-RC3-SNAPSHOT", appInfo.getVersion());
     }
 }
