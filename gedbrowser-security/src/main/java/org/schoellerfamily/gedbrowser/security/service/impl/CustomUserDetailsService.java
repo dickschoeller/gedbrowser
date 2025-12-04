eb236e9238562aeb423bb74bd22f2109603f6fc8
Richard Schoeller <dickschoeller@gmail.com>
2025-12-04 12:55:26 -0500

    Reorganize imports

--- gedbrowser-security/src/main/java/org/schoellerfamily/gedbrowser/security/service/impl/CustomUserDetailsService.java
+++ gedbrowser-security/src/main/java/org/schoellerfamily/gedbrowser/security/service/impl/CustomUserDetailsService.java
@@ -1,18 +1,15 @@
 package org.schoellerfamily.gedbrowser.security.service.impl;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.schoellerfamily.gedbrowser.security.model.SecurityUser;
 import org.schoellerfamily.gedbrowser.security.model.SecurityUsers;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
