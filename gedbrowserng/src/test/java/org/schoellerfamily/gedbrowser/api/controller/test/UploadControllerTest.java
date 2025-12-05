8891d7c0dfeeef4bd398a456666a1330f6f5d8e6
Richard Schoeller <dickschoeller@gmail.com>
2025-12-05 14:13:02 -0500

    Rework mock structure

--- gedbrowserng/src/test/java/org/schoellerfamily/gedbrowser/api/controller/test/UploadControllerTest.java
+++ gedbrowserng/src/test/java/org/schoellerfamily/gedbrowser/api/controller/test/UploadControllerTest.java
@@ -1,6 +1,11 @@
 package org.schoellerfamily.gedbrowser.api.controller.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -8,13 +13,10 @@ import java.net.HttpURLConnection;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.schoellerfamily.gedbrowser.api.controller.UploadController;
 import org.schoellerfamily.gedbrowser.api.datamodel.ApiHead;
+import org.schoellerfamily.gedbrowser.api.service.storage.FileSystemStorageService;
 import org.schoellerfamily.gedbrowser.api.service.storage.StorageService;
 import org.schoellerfamily.gedbrowser.persistence.mongo.gedconvert.GedObjectToGedDocumentMongoConverter;
 import org.schoellerfamily.gedbrowser.persistence.mongo.loader.GedDocumentFileLoader;
@@ -40,25 +42,19 @@ public class UploadControllerTest {
     private MockMvc mockMvc;
 
     /** */
-    @Mock
-    private StorageService service;
+    private StorageService service = mock(FileSystemStorageService.class);
 
     /** */
-    @Mock
-    private GedDocumentFileLoader loader;
+    private GedDocumentFileLoader loader = mock(GedDocumentFileLoader.class);
 
     /** */
-    @Mock
-    private GedObjectToGedDocumentMongoConverter toDocConverter;
+    private GedObjectToGedDocumentMongoConverter toDocConverter = mock(GedObjectToGedDocumentMongoConverter.class);
 
     /** */
-    @Mock
-    private RepositoryManagerMongo repositoryManager;
+    private RepositoryManagerMongo repositoryManager = mock(RepositoryManagerMongo.class);
 
     /** */
-    @Spy
-    @InjectMocks
-    private UploadController controller = new UploadController(loader, toDocConverter, repositoryManager, service);
+    private UploadController controller = spy(new UploadController(loader, toDocConverter, repositoryManager, service));
 
     /** */
     @Before
@@ -75,10 +71,12 @@ public class UploadControllerTest {
     public void testUploadController() throws Exception {
         final MockMultipartFile mockMultipartFile = new MockMultipartFile(
                 "file", "mini-schoeller.ged", "multipart/form-data", is);
-        Mockito.doNothing().when(service).store(mockMultipartFile);
+        doNothing()
+            .when(service).store(mockMultipartFile);
         final ApiHead o =
                 new ApiHead("type", "string that you would never get");
-        Mockito.doReturn(o).when(controller).readOne("mini-schoeller");
+        doReturn(o)
+            .when(controller).readOne(anyString());
         final MvcResult result = mockMvc.perform(
                 MockMvcRequestBuilders
                     .fileUpload("/v1/upload")
@@ -90,7 +88,7 @@ public class UploadControllerTest {
                 .andReturn();
         final ObjectMapper mapper = new ObjectMapper();
         final String json = mapper.writeValueAsString(o);
-        assertEquals("head object doesn't match",
-                json, result.getResponse().getContentAsString());
+        assertEquals(json, result.getResponse().getContentAsString(),
+            "head object doesn't match");
     }
 }
