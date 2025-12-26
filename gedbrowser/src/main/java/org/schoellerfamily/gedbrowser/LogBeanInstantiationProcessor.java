39ae7f306c3cb0bacb347d7f66b80c504c52a02e
Richard Schoeller <dickschoeller@gmail.com>
2025-12-26 14:12:54 -0500

    Remove unnecessary @Nonnull

--- gedbrowser/src/main/java/org/schoellerfamily/gedbrowser/LogBeanInstantiationProcessor.java
+++ gedbrowser/src/main/java/org/schoellerfamily/gedbrowser/LogBeanInstantiationProcessor.java
@@ -2,7 +2,6 @@ package org.schoellerfamily.gedbrowser;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,21 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 public class LogBeanInstantiationProcessor implements BeanPostProcessor {
 
     @Override
-    public Object postProcessBeforeInitialization(
-        @NonNull
-        final Object bean,
-        @NonNull
-        final String beanName) throws BeansException {
+    public Object postProcessBeforeInitialization(final Object bean, final String beanName) throws BeansException {
         // No action needed before initialization for this purpose
         return bean;
     }
 
     @Override
-    public Object postProcessAfterInitialization(
-        @NonNull
-        final Object bean,
-        @NonNull
-        final String beanName) throws BeansException {
+    public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
         log.debug("Bean instantiated: Name='{}', Class='{}'", beanName, bean.getClass().getName());
         return bean;
     }
