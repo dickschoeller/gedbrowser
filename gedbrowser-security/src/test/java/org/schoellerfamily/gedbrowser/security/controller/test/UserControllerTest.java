d9a9ff101cd3c33fec8d2ee5ae0b55d69ae99e12
Richard Schoeller <dickschoeller@gmail.com>
2026-01-14 16:43:32 -0500

    warning

--- gedbrowser-security/src/test/java/org/schoellerfamily/gedbrowser/security/controller/test/UserControllerTest.java
+++ gedbrowser-security/src/test/java/org/schoellerfamily/gedbrowser/security/controller/test/UserControllerTest.java
@@ -25,7 +25,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
