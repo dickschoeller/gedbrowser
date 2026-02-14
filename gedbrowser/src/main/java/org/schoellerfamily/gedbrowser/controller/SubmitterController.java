0e98039774ebe7d3cd1a9f7128f75ca1a7a12761
Richard Schoeller <dickschoeller@gmail.com>
2026-02-13 19:52:21 -0500

    Fix gedbrowser controllers

--- gedbrowser/src/main/java/org/schoellerfamily/gedbrowser/controller/SubmitterController.java
+++ gedbrowser/src/main/java/org/schoellerfamily/gedbrowser/controller/SubmitterController.java
@@ -15,10 +15,10 @@ import org.schoellerfamily.gedbrowser.renderer.application.ApplicationInfo;
 import org.schoellerfamily.geoservice.client.GeoServiceClient;
 import org.schoellerfamily.geoservice.keys.KeyManager;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,7 +27,7 @@ import lombok.extern.slf4j.Slf4j;
  *
  * @author Dick Schoeller
  */
-@RestController
+@Controller
 @Slf4j
 public class SubmitterController extends GeoDataController {
     /** Location of gedbrowser configuration files. */
