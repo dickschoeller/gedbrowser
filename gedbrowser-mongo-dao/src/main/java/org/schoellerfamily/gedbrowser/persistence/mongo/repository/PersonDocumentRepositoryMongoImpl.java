3e5e13ba2db185c4c193f81d39dd1e63a5312378
Richard Schoeller <dickschoeller@gmail.com>
2026-02-18 12:45:12 -0500

    Unused import

--- gedbrowser-mongo-dao/src/main/java/org/schoellerfamily/gedbrowser/persistence/mongo/repository/PersonDocumentRepositoryMongoImpl.java
+++ gedbrowser-mongo-dao/src/main/java/org/schoellerfamily/gedbrowser/persistence/mongo/repository/PersonDocumentRepositoryMongoImpl.java
@@ -2,7 +2,6 @@ package org.schoellerfamily.gedbrowser.persistence.mongo.repository;
 
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
